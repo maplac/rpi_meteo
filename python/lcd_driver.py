2688fbd0d7ecbd6de42c3f24c1ad521cadfd3a29
Martin Placek <maplac@seznam.cz>
2022-06-19 18:37:35 +0200

    LCD driver shows values from remote sensors

--- python/lcd_driver.py
+++ python/lcd_driver.py
@@ -7,6 +7,11 @@ bme280_t = 0
 bme280_p = 0
 bme280_h = 0
 
+dht22_valid = False
+dht22_t = 0
+dht22_v = 0
+dht22_h = 0
+
 try:
     client = InfluxDBClient(host='192.168.0.104', port=8086, database='home')
     res = client.query('SELECT mean("temperature"),mean("pressure"),mean("humidity") '
@@ -17,6 +22,16 @@ try:
         bme280_p = point['mean_1']
         bme280_h = point['mean_2']
         bme280_valid = True
+        break
+        
+    res = client.query('SELECT mean("temperature"),mean("humidity"),mean("voltage") '
+                       'FROM "test" WHERE ("name" = \'dht22\') AND time >= now() - 5m')
+    
+    for point in res.get_points():
+        dht22_t = point['mean']
+        dht22_h = point['mean_1']
+        dht22_v = point['mean_2']
+        dht22_valid = True
         break
 except Exception as e:
     print("Error: ", e.__class__, ", ", e)
@@ -35,7 +50,15 @@ if bme280_valid:
     lcd.write_string("{:.1f}".format(bme280_t) + chr(176) + "C ")
     lcd.write_string("{:.1f}".format(bme280_h) + chr(37) + " ")
     lcd.write_string("{:.0f}".format(bme280_p) + "hPa")
-    
+else:
+    lcd.write_string("no data")
+
+lcd.crlf()
+
+if dht22_valid:
+    lcd.write_string("{:.1f}".format(dht22_t) + chr(176) + "C ")
+    lcd.write_string("{:.1f}".format(dht22_h) + chr(37) + " ")
+    lcd.write_string("{:.2f}".format(dht22_v) + "V")
 else:
     lcd.write_string("no data")
 
